9bd86e86eae1c5e3a61598380550d3e8bc4fa97e
CapBlack222 <nish.gup.446@gmail.com>
2026-02-09 21:40:17 -0500

    error handling updated

--- src/main/java/tech/gupnish/moodmate/errors/ApplicationExceptionHandler.java
+++ src/main/java/tech/gupnish/moodmate/errors/ApplicationExceptionHandler.java
@@ -1,23 +1,39 @@
 package tech.gupnish.moodmate.errors;
 
+import java.time.LocalDateTime;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class ApplicationExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleIllegalArgumentException(IllegalArgumentException ex) {
-        return "<h1>" + ex.getMessage() + "</h1>";
+    public ErrorResponse handleIllegalArgumentException(IllegalArgumentException ex, HttpServletRequest request) {
+        return new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI()
+            );
     }
 
     @ExceptionHandler(MoodNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleMoodNotFoundException(MoodNotFoundException ex) {
-        return "<h1>" + ex.getMessage() + "</h1>";
+    public ErrorResponse handleMoodNotFoundException(MoodNotFoundException ex, HttpServletRequest request) {
+        return new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI()
+            );
     }
 
 }
